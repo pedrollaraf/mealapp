f06a8f3c742ef9e0329113c6d0c158f4ce415d16
Pedro L Freitas <pllsatierf@gmail.com>
2022-01-05 00:27:58 -0300

    Add dependencies
    Added dependencies to build gradle and configure everything

--- app/src/main/java/com/pedrollaraf/mealapp/presentation/categories/ui/MainActivity.kt
+++ app/src/main/java/com/pedrollaraf/mealapp/presentation/categories/ui/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.pedrollaraf.mealapp
+package com.pedrollaraf.mealapp.presentation.categories.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pedrollaraf.mealapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
